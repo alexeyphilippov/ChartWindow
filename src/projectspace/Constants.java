ace114af9b789ba9328456be9f777bae3d5e3e4f
aleksejfilippov <ahtamar909@gmail.com>
2018-04-14 02:27:31 +0300

    alert and Constants.

---
+++ src/projectspace/Constants.java
@@ -0,0 +1,16 @@
+package projectspace;
+
+import javafx.scene.layout.VBox;
+
+public interface Constants {
+    public static final VBox vbox = new VBox();
+    public static final int NUMBER_OF_DOTS = 20;
+    public static final int USEFULL_INFO_START_INDEX = 14;
+    public static final int USEFULL_INFO_END_INDEX = 21;
+    public static final int VALUE_LOWER_BOUND = 27;
+    public static final int VALUE_UPPER_BOUND = 29;
+    public static final double SPACE_BETWEEN_TWO_Xs  = 1;
+    public static final double SPACE_BETWEEN_TWO_Ys  = 0.1;
+
+
+}
